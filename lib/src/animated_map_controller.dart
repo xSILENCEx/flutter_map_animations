8d5538264f5c6b4de131ff98182abf940b7cd0b2
Mike <894659061@qq.com>
2023-08-01 15:43:09 +0800

    暂存

--- lib/src/animated_map_controller.dart
+++ lib/src/animated_map_controller.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:flutter_map_animations/src/animation_extensions.dart';
+import 'package:flutter_map_animations/src/animation_id.dart';
 import 'package:flutter_map_animations/src/lat_lng_tween.dart';
 import 'package:latlong2/latlong.dart';
 
